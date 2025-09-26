c82b293966c7b935e26b9c272e9b77b792124ce4
armoredvortex <66690593+ArmoredVortex@users.noreply.github.com>
2025-09-27 02:04:56 +0530

    refactor: clean up imports and simplify content in Home component, fix scroll on homepage

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,24 +1,11 @@
 import GlitchText from "@/components/GlitchText";
-import { AnimatedText } from "@/components/AnimatedText";
-import { Button } from "@/components/ui/button";
-import Link from "next/link";
-import {
-  ArrowRight,
-  Shield,
-  Users,
-  Code,
-  Trophy,
-  Calendar,
-  MessageCircle,
-  Star,
-  School,
-} from "lucide-react";
+import { Shield, Users, School } from "lucide-react";
 
 export default function Home() {
   return (
     <div className="w-full max-w-6xl text-center flex flex-col items-center">
       {/* Hero Section */}
-      <div className="h-screen flex flex-col justify-center items-center w-full relative overflow-hidden">
+      <div className="min-h-[80vh] flex flex-col justify-center items-center w-full relative overflow-hidden">
         <div className="relative z-10">
           <GlitchText
             speed={1}
@@ -34,11 +21,7 @@ export default function Home() {
             </h2>
             <p className="text-lg md:text-xl text-foreground/90 leading-relaxed">
               Information Security division of Cyberlabs IIT (ISM) Dhanbad. We
-              are a team of passionate individuals dedicated to exploring the
-              realms of cybersecurity.
-              <br />
-              <br />
-              We organize workshops, host events, and participate in various CTF
+              organize workshops, host events, and participate in various CTF
               competitions to spread and enhance our skills and knowledge in the
               field of information security.
             </p>
@@ -60,16 +43,6 @@ export default function Home() {
           </div>
         </div>
       </div>
-
-      {/* About Section */}
-      {/* <div className="min-h-screen flex flex-col justify-center items-center w-full py-16 px-8">
-        <div className="glass rounded-xl p-8 max-w-4xl mx-auto">
-          <h2 className="text-2xl md:text-3xl font-bold text-primary mb-6">About Us</h2>
-          <p className="text-lg md:text-xl text-foreground/90 leading-relaxed">
-            We are the Information Security Division of CyberLabs, dedicated to mastering the art and science of cybersecurity. Our team actively participates in Capture The Flag (CTF) competitions and bug bounty programs, where we learn, exploit, and defend systems. Beyond competing, we focus on both offensive and defensive security practices—hacking ethically to understand vulnerabilities and protecting systems to safeguard digital assets. Every year, we also host a global CTF, fostering collaboration and knowledge sharing within the cybersecurity community.
-          </p>
-        </div>
-      </div> */}
     </div>
   );
 }
